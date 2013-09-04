b40a4bac26e03769e8009b8ccc73ed60161c3b4b
gentle0219 <gentle0219@hotmail.com>
2013-09-04 11:43:48 +0800

    added page_top_header

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,6 @@
 class Event < ActiveRecord::Base
   attr_accessible :name
-  has_many :luxe_registries
+  has_many :luxe_registries, 										:dependent => :destroy
 
   has_many :offer_events
   has_many :offers, :through => :offer_events
