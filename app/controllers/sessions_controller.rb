f637115687ba0fb43d203a11a0860acb8bf863fa
Chui Gwangming <gentle0219@hotmail.com>
2013-09-13 11:05:03 +0800

    modify authorize.net.rb

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,22 @@
 class SessionsController < ApplicationController
-
+  
+  AUTHENTICATE_URL = 'http://198.57.244.119/luxesocialevents/authorize.php'
+  TEST_AUTHENTICATE_URL = 'http://192.168.0.78/workspace/drupal/authorize.php'
+  
   def new    
   end
  
   def create
     user = User.find_by_mail(params[:session][:email].downcase)
+    
+    option = user.pass
+    pswd = params[:session][:password]
 
-    if user #&& user.authenticate(params[:session][:password])
+    params = {'pswd'=>pswd, 'option'=>option[0,12]}
+    x = Net::HTTP.post_form(URI.parse(AUTHENTICATE_URL), params)
+    pswd_token = x.body
+
+    if user && pswd_token == option
       session[:current_user_id] = user.uid
       flash[:success] = "Welcome"
       redirect_to root_path
--- config/initializers/authorize_net.rb
+++ config/initializers/authorize_net.rb
@@ -4,8 +4,8 @@ authorize_net_settings = YAML::load(File.open("#{Rails.root.to_s}/config/gateway
 ActiveMerchant::Billing::Base.mode = (['production'].include?(Rails.env) ? :production : :test)
 
 ::GATEWAY = ActiveMerchant::Billing::AuthorizeNetGateway.new(
-  :login    => '62tUsQ4Jt5',
-  :password => '5zS9G58cXPHf83wf',
+  :login    => (!['production'].include?(Rails.env)) ? '62tUsQ4Jt5' : ENV[AUTHORIZE_NET_LOGIN_ID],
+  :password => (!['production'].include?(Rails.env)) ? '5zS9G58cXPHf83wf' : ENV[AUTHORIZE_NET_PASSWORD],
   :test     => (!['production'].include?(Rails.env))
   # :test     => false
 )
